c069043e5950987d42f9c42ac4e824beb07c51c7
MykolaLukianovych <lukjanovych@gmail.com>
2025-02-01 21:10:27 +0200

    localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
@@ -6,12 +6,27 @@ import ContactForm from "./components/ContactForm/ContactForm";
 
 
 function App() {
-  const [contacts, setContacts] = useState([
+  const [contacts, setContacts] = useState(() => {
+    const lastContactsList = window.localStorage.getItem("lastContactsList");
+
+    if (lastContactsList !== null) {
+      return JSON.parse(lastContactsList);
+    }
+
+    return [
   {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
   {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
   {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
   {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-]);
+  ]
+  });
+  
+
+  useEffect(() => {
+    window.localStorage.setItem("lastContactsList", JSON.stringify(contacts))
+  }, [contacts]);
+
+
   const [searchFriend, setSearchFriend] = useState("");
 
   const handleAddContact = (newContact) => {
